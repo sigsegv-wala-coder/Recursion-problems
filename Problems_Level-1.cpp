2865b818cebbc7424f0ff54d96fc373cd1880bd5
Harsh Raj <54831615+harsh17087@users.noreply.github.com>
2021-02-27 21:54:48 +0530

    Update Problems_Level-1.cpp

--- Problems_Level-1.cpp
+++ Problems_Level-1.cpp
@@ -136,8 +136,26 @@ int main(){
 }
 //*******************************************************************************************************************************************************
 
+/* Problem 6 : Multiply numbers without using multiply/divide operator recursively
 
+Approach : if we multiply 5 x 6 then, we are basically adding 5 till 6 times. */
 
+#include<iostream>
+using namespace std;
+
+int multiplyNumbers(int m, int n) {
+     if(n==0){
+        return 0;
+    }
+    return m+multiplyNumbers(m,n-1);
+}
+int main(){
+	int m,n;
+	cin>>m>>n;
+	multiplyNumbers(m,n);
+}
+
+//********************************************************************************************************************************************************
 
 
 
