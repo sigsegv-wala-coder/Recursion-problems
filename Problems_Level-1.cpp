338b74e844069a4bf159962f94fdd9fc7b11b06b
Harsh Raj <54831615+harsh17087@users.noreply.github.com>
2021-02-27 14:17:11 +0530

    Update Problems_Level-1.cpp

--- Problems_Level-1.cpp
+++ Problems_Level-1.cpp
@@ -51,3 +51,59 @@ int main(){
 
 //*********************************************************************************************************************************************
 
+/* Problem 3 : Count the digits recursively
+
+Approach : If number is less than 10 , return 1 . Call the recursion after dividing the number by 10 i.e. excluding one digit and subsequently
+            add 1 as we're excluding that digit after dividing by 10 */
+
+#include<iostream>
+using namespace std;
+
+int digits(int n){
+    if(n<10){
+        return 1;
+    }
+    return 1+digits(n/10);
+}
+
+int main(){
+  int n;
+  cin>>n;
+  cout<<digits(n)<<endl;
+}
+
+//*************************************************************************************************************************************************
+
+/* Problem 4 : Check whether the given array is sorted recursively.
+
+Approach : If array consists of 0 or 1 element, it is already sorted. Now check for 1st and 2nd element.
+           If 1st > 2nd , return false . Now call the recursive function for the rest of the array by passing
+           arr+1 and size-1 .                  */
+           
+#include <iostream>
+using namespace std;
+
+bool check_sorted(int arr[] , int size){
+    if(size==0 || size==1){
+        return true;
+    }
+    if(arr[0]>arr[1]){
+        return false;
+    }
+    return check_sorted(arr+1,size-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    // int *p=new int[n];
+    // for(int i=0;i<n;i++){
+    //     cin>>p[i];
+    // }
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<check_sorted(arr,n)<<endl;
+}
+
+//*************************************************************************************************************************************************
