a56cdf63ccd9e3f2c4c6b6dd6d1db241f3f74a1a
Harsh Raj <54831615+harsh17087@users.noreply.github.com>
2021-02-27 13:22:03 +0530

    Update Problems_Level-1.cpp

--- Problems_Level-1.cpp
+++ Problems_Level-1.cpp
@@ -1,8 +1,8 @@
 //****************************************************************************************************************************************
 
-// Problem 1 : Write a program to find x to the power n (i.e. x^n). Take x and n from the user. Do it recursively.
+/* Problem 1 : Write a program to find x to the power n (i.e. x^n). Take x and n from the user. Do it recursively.
 
-// Approach : Base case should be if power is 0 , return 1. Inductive relation should be x^n = x * x^(n-1)
+ Approach : Base case should be if power is 0 , return 1. Inductive relation should be x^n = x * x^(n-1) */
 
 #include<iostream>
 using namespace std;
@@ -23,4 +23,31 @@ int main(){
 
 }
 //******************************************************************************************************************************************
+
+/* Problem 2 : Print numbers from 1 to n in increasing order recursively.
+
+Approach : Base case should print 1 and return. */
+
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    if(n == 1){
+        cout<<n<<" ";
+        return;
+    }
+    
+    print(n - 1);
+    cout << n << " ";
+}
+
+int main(){
+  int n;
+  cin>>n;
+  print(n);
+}
+
+// One thing to note is if we interchange line 40 and 41, the order reverses. So, little attentive while calling the recursive function.
+
+//*********************************************************************************************************************************************
 
